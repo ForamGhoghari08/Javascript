80460615d3222a89d3f3d387bf0935a79b108318
ForamGhoghari08 <ghoghariforam8@gmail.com>
2024-04-17 23:03:07 +0530

    Find the Remainder

---
+++ Find the Remainder/index.js
@@ -0,0 +1,11 @@
+let firstVariable = 10;
+let secondVariable = 5;
+let thirdVariable = 3;
+
+let sum = firstVariable + secondVariable;
+
+let divisionResult = sum / thirdVariable;
+
+let remainder = sum % thirdVariable;
+
+console.log(remainder); 
