b9b806bd396d6130ac8507e4ffc70ca7bd1762e5
ForamGhoghari08 <ghoghariforam8@gmail.com>
2024-04-17 22:39:18 +0530

    Square and Sum

---
+++ Square and Sum/index.js
@@ -0,0 +1,13 @@
+let one = 1;
+let two = 2;
+let three = 3;
+
+let squareOne = one * one; 
+let squareTwo = two * two; 
+let squareThree = three * three; 
+
+let sum = squareOne + squareTwo + squareThree; 
+
+
+console.log(sum); 
+
