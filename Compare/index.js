6a05c1fc16ecc21f5410b1c738c561dba32c8718
ForamGhoghari08 <ghoghariforam8@gmail.com>
2024-04-21 23:51:46 +0530

    Compare

---
+++ Compare/index.js
@@ -0,0 +1,13 @@
+// Sample input
+let num1 = 4;
+let num2 = 8;
+let num3 = 5;
+
+// Output of the first operation
+console.log(num1 > num2); // false
+
+// Adding num3 to num1
+num1 += num3;
+
+// Output of the second operation
+console.log(num1 > num2); // true
