393c511bbafebb50a93075139a1ec983e88f9742
ForamGhoghari08 <ghoghariforam8@gmail.com>
2024-04-21 23:49:36 +0530

    Find even or odd

---
+++ Find even or odd/index.js
@@ -0,0 +1,9 @@
+// Input number
+let number = 4;
+
+// Check if the number is odd or even
+if (number % 2 === 0) {
+    console.log("It's an even number.");
+} else {
+    console.log("It's an odd number.");
+}
