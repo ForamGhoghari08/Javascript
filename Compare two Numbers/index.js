75f56ea4d7d18df9fbedee99459ce0a1e45079b3
ForamGhoghari08 <ghoghariforam8@gmail.com>
2024-04-21 23:54:06 +0530

    Compare two numbers

---
+++ Compare two Numbers/index.js
@@ -0,0 +1,12 @@
+// Sample input
+let num1 = 4;
+let num2 = 16;
+
+// Output of the first operation
+console.log(num1 > num2); // false
+
+// Output of the second operation
+console.log(num1 < num2); // true
+
+// Output of the third operation
+console.log(num1 == num2); // false
