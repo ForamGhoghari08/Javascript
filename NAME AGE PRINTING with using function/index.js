d09ea17342b5a7c5acd0224973f584b7f62cfd6d
ForamGhoghari08 <ghoghariforam8@gmail.com>
2024-04-28 17:18:16 +0300

    NAME AGE PRINTING with using function

---
+++ NAME AGE PRINTING with using function/index.js
@@ -0,0 +1,9 @@
+function printNameAndAge(name, age) {
+    console.log(name, age);
+    console.log(typeof name, typeof age);
+}
+
+let name = "Foram Ghoghari";
+let age = 15;
+
+printNameAndAge(name, age);
