342df86698061941fa02a168b573e24e1dfcc6c5
ForamGhoghari08 <ghoghariforam8@gmail.com>
2024-04-28 17:56:20 +0300

    Even Sum Below N

---
+++ Even Sum Below N/index.js
@@ -0,0 +1,10 @@
+function sumOfEvenNumbers(num) {
+    let sum = 0;
+    for (let i = 2; i <= num; i += 2) {
+        sum += i;
+    }
+    return sum;
+}
+
+let num = 6;
+console.log(sumOfEvenNumbers(num)); 
