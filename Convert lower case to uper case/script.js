54e940cf172be95de2936606d17efc683b5a3156
ForamGhoghari08 <ghoghariforam8@gmail.com>
2024-05-05 18:15:07 +0530

    Convert lower case to upper case

---
+++ Convert lower case to uper case/script.js
@@ -0,0 +1,5 @@
+function convertToUppercase() {
+    let inputText = document.getElementById('inputText').value;
+    let outputDiv = document.getElementById('output');
+    outputDiv.textContent = inputText.toUpperCase();
+}
